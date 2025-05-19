90787830b4321c073b83a1432f000093e455ea30
peaceofsense <72244031+peaceofsense@users.noreply.github.com>
2025-05-19 10:26:14 +0200

    Flake install home-manager

--- flake.nix
+++ flake.nix
@@ -1,10 +1,15 @@
 {
 
   description = "yes";
-  
+
   inputs = {
     nixpkgsStable.url = "nixpkgs/nixos-24.11"; # Change this to update version
     nixpkgsUnstable.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgsStable";
+    };
+
     solaar = {
       url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz"; # For latest stable version
       #url = "https://flakehub.com/f/Svenum/Solaar-Flake/0.1.1.tar.gz"; # uncomment line for solaar version 1.1.13
@@ -20,8 +25,8 @@
       inputs.nixpkgs.follows = "nixpkgsUnstable";
     };
   };
-  
-  outputs = { self, nixpkgsStable, nixpkgsUnstable, astal, ags, solaar,... } @ inputs:
+
+  outputs = { self, nixpkgsStable, nixpkgsUnstable, home-manager, astal, ags, solaar,... } @ inputs:
     let
       lib = nixpkgsStable.lib; # It is like pass nixpkgs to this var
       system = "x86_64-linux";
@@ -30,7 +35,7 @@
       pkgsUnstable = nixpkgsUnstable.legacyPackages.${system};
       username = "peaceofsense";
     in {
-    packages.${system}.default = ags.lib.bundle { 
+    packages.${system}.default = ags.lib.bundle {
       inherit pkgsUnstable;
       src = ./.;
       name = "my-shell"; # name of executable
@@ -46,16 +51,22 @@
 
     nixosConfigurations = {
       monolith = lib.nixosSystem { # System Name
-        inherit system;        
-	      modules = [ 
-            ./configuration.nix 
-            solaar.nixosModules.default 
+        inherit system;
+	      modules = [
+            ./configuration.nix
+            solaar.nixosModules.default
           ];
         specialArgs = {
           inherit username;
           inherit pkgsUnstable;
           inherit astal;
         };
+      };
+    };
+    homeConfigurations = {
+      peaceofsense = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [./home.nix];
       };
     };
   };
